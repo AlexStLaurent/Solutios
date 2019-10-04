0522581898106d6951c2f1c2c5a5624f7677d544
Didier <1657485@csimple.org>
2019-10-04 12:24:05 -0400

    MVC

---
+++ Solutios/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Solutios.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
