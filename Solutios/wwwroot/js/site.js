2fd235dbf79eded1e558c59b59cc8d5648bf4949
Didier <1657485@csimple.org>
2019-10-08 14:23:34 -0400

    Modify layout links

--- Solutios/wwwroot/js/site.js
+++ Solutios/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $(".add-row").click(function () {
+        var markup = "<tr><td><input type='textbox' name='record'></td><td><input type='textbox' name='record'></td><td><input type='textbox' name='record'><button type='button' class='delete'>Delete Row</button></td></tr>";
+        $("table tbody").append(markup);
+    });
+
+    // Find and remove selected table rows
+    $(".delete-row").click(function () {
+        $("table tbody").find('input[name="record"]').each(function () {
+            if ($(this).is(":checked")) {
+                $(this).parents("tr").remove();
+            }
+        });
+    });
+});
