c0484cea6dbd2667537146f56eb53506370f947a
devan shaw <noshd31@gmail.com>
2019-11-18 16:53:26 -0500

    expenses en progres

---
+++ Solutios/wwwroot/js/ExpenseTable.js
@@ -0,0 +1,26 @@
+﻿var counter = 1;
+$(function () {
+	//ajout d'une ligne
+	$('#add').click(function () {
+		$('<tr id="tablerow' + counter + '">' +
+				'<td>'+
+					'un test d\'une dépense'+
+					'</td>'+
+			'<td>' +
+			'<input class="form-control" name="subName ' + counter + '" type="text" value="10,000" />' +
+					'</td>'+
+				'<td>'+
+					'<a class="btn btn-success text-white" name="modifybtn0">Sauvegarder/Modifier</a>'+
+				'</td>'+
+			'</tr>').appendTo('#tableSubExpenses');
+		counter++;
+		return false;
+	});
+});
+function removeTr(index) {
+	if (counter > 1) {
+		$('#tablerow' + index).remove();
+		counter--;
+	}
+	return false;
+}
