80b29d8589e8292ad1343dbc388e86885ad18afc
Didier <1657485@csimple.org>
2019-12-06 08:32:39 -0500

    Modif Graph

--- Solutios/Models/ViewModel/ViewGraph.cs
+++ Solutios/Models/ViewModel/ViewGraph.cs
@@ -9,6 +9,8 @@ namespace Solutios.Models.ViewModel
     {
         public string label { get; set; }
         public string data { get; set; }
+        public string dépense { get; set; }
+        public string colordepense { get; set; }
         public string color { get; set; }
         public string soumissionColor { get; set; }
         public string soumission { get; set; }
